40caf363adda7cf840874eb83f3f4fff6cf5e31a
Rammina <AltinaSchwarzer@gmail.com>
2020-02-15 21:23:24 +0800

    ADDED app loader react component

---
+++ src/components/AppLoader/AppLoader.js
@@ -0,0 +1,25 @@
+/*
+// Screen loader
+setTimeout(function () {
+	if (document.readyState === 'loading') {  // Loading hasn't finished yet
+	  document.addEventListener('DOMContentLoaded', function () {
+		document.querySelector(".loader-container").classList.add("no-display");
+
+	  });
+	} //Loading has already finished
+	else if(document.readyState === "complete" || document.readyState === "interactive" || document.readyState === "loaded"){
+	  console.log(" HTML loaded");
+	  document.querySelector(".loader-container").classList.add("no-display"); //Get rid of the loader
+	}
+  }, 600);
+  */
+
+import React from "react";
+class AppLoader extends React.Component {
+  state = {};
+  componentDidMount() {}
+  render() {
+    return <div></div>;
+  }
+}
+export default AppLoader;
