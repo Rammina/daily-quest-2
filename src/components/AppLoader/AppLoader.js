cd18cd6d46cd2d8c4ba3756ef9fd4c969864ee4f
Rammina <altinaschwarzer@gmail.com>
2020-02-19 19:12:05 -0700

    Added loader-star

--- src/components/AppLoader/AppLoader.js
+++ src/components/AppLoader/AppLoader.js
@@ -27,7 +27,7 @@ class AppLoader extends React.Component {
         this.setState({ loaderFadeClass: "no-display" });
         this.hideLoader(300);
       }
-    }, 500);
+    }, 5000000);
   }
 
   hideLoader = delay => {
@@ -46,11 +46,20 @@ class AppLoader extends React.Component {
     }
     return (
       <div className={`loader-container ${this.state.loaderFadeClass || null}`}>
-        <div className="loader-message-container">
-          <div className="loader-text">
-            <p className="loader-p">Loading...</p>
-          </div>
+        <div class="loader loader-2">
+          <svg
+            class="loader-star"
+            xmlns="http://www.w3.org/2000/svg"
+            version="1.1"
+          >
+            <polygon
+              points="29.8 0.3 22.8 21.8 0 21.8 18.5 35.2 11.5 56.7 29.8 43.4 48.2 56.7 41.2 35.1 59.6 21.8 36.8 21.8 "
+              fill="#18ffff"
+            />
+          </svg>
+          <div class="loader-circles"></div>
         </div>
+        {/* <p className="loader-p">Loading...</p> */}
       </div>
     );
   }
