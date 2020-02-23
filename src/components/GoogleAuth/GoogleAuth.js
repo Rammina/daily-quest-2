f2374dbda2ce30eb5d58254bed8723eae944cfbf
Rammina <AltinaSchwarzer@gmail.com>
2020-02-23 18:58:33 +0800

    added design for the Google auth button

--- src/components/GoogleAuth/GoogleAuth.js
+++ src/components/GoogleAuth/GoogleAuth.js
@@ -12,8 +12,11 @@ class GoogleAuth extends React.Component {
           scope: "email"
         })
         .then(() => {
+          // gives a ref to the auth instance
           this.auth = window.gapi.auth2.getAuthInstance();
-          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          // update component level state, check if user is signed in or not
+          this.onAuthChange();
+          // listen for any changes in sign in status, update state
           this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
@@ -31,13 +34,33 @@ class GoogleAuth extends React.Component {
     this.auth.signOut();
   };
 
+  renderGoogleSignButton = (text, cb) => {
+    return (
+      <button
+        className="ui red google button"
+        onClick={() => {
+          if (cb) {
+            cb();
+          }
+        }}
+      >
+        <i className="google icon" />
+        {text}
+      </button>
+    );
+  };
+
   renderAuthButton = () => {
     if (this.state.isSignedIn === null) {
-      return <div></div>;
+      return null;
     } else if (this.state.isSignedIn) {
-      return <div>signed in</div>;
+      return this.renderGoogleSignButton("Sign Out", () => {
+        this.onSignOutClick();
+      });
     } else {
-      return <div>not signed in</div>;
+      return this.renderGoogleSignButton("Sign In w/ Google", () => {
+        this.onSignInClick();
+      });
     }
   };
 
