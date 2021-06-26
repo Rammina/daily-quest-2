b277c1271187fbcb4db5124e9ee14c198a33427d
Rammina <49326578+Rammina@users.noreply.github.com>
2021-06-26 13:38:23 +0800

    changing styles that dont follow flatUI design on the navbar

--- src/test/testUtils.js
+++
@@ -1,9 +0,0 @@
-export const findByTestAttributes = (wrapper, value) => {
-	return wrapper.find(`[data-test="${value}"]`);
-};
-
-export const resizeWindow = (x, y) => {
-	window.innerWidth = x;
-	window.innerHeight = y;
-	window.dispatchEvent(new Event('resize'));
-}
